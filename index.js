647ec44517b62c6637f12568d93cac83306fb51b
Joonbie <163788771+Joonbie@users.noreply.github.com>
2024-03-26 13:29:42 +0800

    Added Project Cards and linking to project

---
+++ index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ["I&apos;m Richard!", "Welcome to My Portfolio!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: false,
+  };
+  var typed = new Typed(".typing", options);
